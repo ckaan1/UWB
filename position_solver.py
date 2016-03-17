9ba58fafba4c380d0351463bf69694354999a113
qizong <qizongasd@gmail.com>
2016-03-17 15:00:40 -0400

    position solver

---
+++ position_solver.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 17 14:54:52 2016
+
+@author: Qizong Wu
+
+"""
+from environment import *
+import datetime
+import numpy as np
+
+## Read fixed anchor position
+
+## 
