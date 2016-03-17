99856ebde5eb196c4587f1085ccccb1c96ce688f
qizong <qizongasd@gmail.com>
2016-03-17 18:00:23 -0400

    NLLS

--- position_solver.py
+++ position_solver.py
@@ -6,30 +6,41 @@
 ####################################################################
 class position_solver:
     
-import environment
-import datetime
+#import environment
+
 import numpy as np
 import scipy.optimize as optimize
 import matplotlib.pylab as plt
 
-## Read fixed anchor position
+## Need to read fixed anchor positions
 
-## Non-Linear Least Squares for LOS
-    def func_1(x, p_FA, d_M) 
-        return (d_M - np.linalg.norm(x-p_FA))^2
+    ## Non-Linear Least Squares for LOS
+    def func_1(x, p_FA, d_M) :
+        num = 3;
+        cost = np.zeros(3,1)
+        for i in range(0,num):
+
+            cost[i] = np.linalg.norm(d_M[i,0:2]) - np.linalg.norm(x-p_FA[i,0:2])
+        return cost
         
-    def residuals(x,p_FA,d_M,PLP):
-        return PLP - func_1(x, p_FA, d_M) 
+    #def residuals(x,p_FA,d_M,PLP):
+    #    return PLP - func_1(x, p_FA, d_M) 
         
-    def NLLS()    # NLLS test
-        N=1000
-        kd_guess= [3.5, 3.5]  # a position guess for mobile tag
+    def NLLS():    # NLLS test
+        
+        x_guess= np.array([3.5, 3.5])  # a position guess for mobile tag
         #p_FA
         #d_M
-        PLP = func_1(x, p_FA, d_M)+(np.random.random(N)-0.5)*0.1
+       
 
         x,cov,infodict,mesg,ier = optimize.leastsq(
-        residuals,kd_guess,args=(p_FA,d_M,PLP),full_output=True,warning=True)
+        residuals,kd_guess,args=(p_FA,d_M),full_output=True,warning=True)
 
         print(x)
+        ## Visualize the results
+        #PLP_fit=func_1(x,p_FA,d_M)
+        #plt.plot()
+        #plt.show()        
+        
+        
         return x
