b875a446d0b154aaeaf715103a0a377999470d9e
qizong <qizongasd@gmail.com>
2016-03-18 01:07:58 -0400

    position solver - Non-Linear Least Square

--- position_solver.py
+++ position_solver.py
@@ -4,43 +4,28 @@
 ## Solver includes Non-linear Least Squares
 ## 
 ####################################################################
-class position_solver:
-    
-#import environment
-
-import numpy as np
-import scipy.optimize as optimize
-import matplotlib.pylab as plt
-
-## Need to read fixed anchor positions
-
-    ## Non-Linear Least Squares for LOS
-    def func_1(x, p_FA, d_M) :
-        num = 3;
-        cost = np.zeros(3,1)
-        for i in range(0,num):
-
-            cost[i] = np.linalg.norm(d_M[i,0:2]) - np.linalg.norm(x-p_FA[i,0:2])
-        return cost
-        
-    #def residuals(x,p_FA,d_M,PLP):
-    #    return PLP - func_1(x, p_FA, d_M) 
-        
-    def NLLS():    # NLLS test
-        
-        x_guess= np.array([3.5, 3.5])  # a position guess for mobile tag
-        #p_FA
-        #d_M
+class Position_solver:
+    # x_guess is the initial guess / p_FA is teh position array of fixed anchors
+    # d_m is the distance vector from mobile target to fixed anchors
+    def __init__(self,x_guess,p_FA,d_M):
        
-
-        x,cov,infodict,mesg,ier = optimize.leastsq(
-        residuals,kd_guess,args=(p_FA,d_M),full_output=True,warning=True)
-
-        print(x)
-        ## Visualize the results
-        #PLP_fit=func_1(x,p_FA,d_M)
-        #plt.plot()
-        #plt.show()        
+		self.NLLS = NLLS_opt(x_guess,p_FA,d_M)
+		
+  
+    # NLLS function
+def func_1(x,p_FA,d_M) :
+    import numpy as np
+    num = len(p_FA)  # num - number of fixed anchors
+    cost = cost = [0.]*num
+    for i in range(0,num):
+        cost[i] = np.linalg.norm(d_M[i,0:2]) - np.linalg.norm(x-p_FA[i,0:2])
+    return cost   
         
+    # NLLS optimization    
+def NLLS_opt(x_guess,p_FA,d_M):   
+    import numpy as np
+    import scipy.optimize as optimize
         
-        return x
+    x = optimize.leastsq(func_1,x_guess,args=(p_FA,d_M))
+
+    return x[0]
