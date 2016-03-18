da6b7bf3a338721a09db186f87cf1b3ef6d5530d
Christian <clkaan@wpi.edu>
2016-03-18 14:18:59 -0400

    pcolor test thing

--- environment.py
+++ environment.py
@@ -17,59 +17,68 @@ class Environment:
 		collisions = {}
 		for o in self.obstacles:
 			collisions[o] = []
-			# Determine obstacle bounds
-			# x = x_min
-			ol1 = self.obstacles[o][0]
-			# y = y_min
-			ol2 = self.obstacles[o][1]
-			# x = x_max
-			ol3 = self.obstacles[o][2]
-			# y = y_max
-			ol4 = self.obstacles[o][3]
-
-			# Determine the min and maximum intersections for both x and y
-			# 3 different configurations for p1 and p2: Horizontal, Vertical, and Sloped
-			# Need to make sure that the intersection exists between p1 and p2
-			# If an intersection doesn't actually exist between p1 and p2, the 
-			# intersection will be either p1 or p2
-
-			# Horizontal line
-			if( p2[1]-p1[1] == 0 ):
-				y_min = p2[1]
-				y_max = p2[1]
-				x_min = min( p1[0],p2[0] )
-				x_max = max( p1[0],p2[0] )
-			# Vertical line
-			elif( p2[0]-p1[0] == 0 ):
-				y_min = min( p1[1],p2[1] )
-				y_max = max( p1[1],p2[1] )
-				x_min = p2[0]
-				x_max = p2[0]
-			# SLoped line
+			if( (p1[0]==p2[0]) and (p1[1]==p2[1])):
+				continue
 			else:
-				# Slope and y-intersect for the line
-				slope = (p2[1]-p1[1])/(p2[0]-p1[0])
-				y_int = p2[1] - slope*p2[0]
+				# Determine obstacle bounds
+				# x = x_min
+				ol1 = self.obstacles[o][0]
+				# y = y_min
+				ol2 = self.obstacles[o][1]
+				# x = x_max
+				ol3 = self.obstacles[o][2]
+				# y = y_max
+				ol4 = self.obstacles[o][3]
 
-				y_min = slope*ol1 + y_int
-				y_max = slope*ol3 + y_int
+				# Determine the min and maximum intersections for both x and y
+				# 3 different configurations for p1 and p2: Horizontal, Vertical, and Sloped
+				# Need to make sure that the intersection exists between p1 and p2
+				# If an intersection doesn't actually exist between p1 and p2, the 
+				# intersection will be either p1 or p2
 
-				x_min = (ol2-y_int)/slope
-				x_max = (ol4-y_int)/slope
+				# Horizontal line
+				if( p2[1]-p1[1] == 0 ):
+					y_min = p2[1]
+					y_max = p2[1]
+					x_min = min( p1[0],p2[0] )
+					x_max = max( p1[0],p2[0] )
+				# Vertical line
+				elif( p2[0]-p1[0] == 0 ):
+					y_min = min( p1[1],p2[1] )
+					y_max = max( p1[1],p2[1] )
+					x_min = p2[0]
+					x_max = p2[0]
+				# SLoped line
+				else:
+					# Slope and y-intersect for the line
+					slope = (p2[1]-p1[1])/(p2[0]-p1[0])
+					y_int = p2[1] - slope*p2[0]
 
-			# Determine the points of collision with the obstacle
-			# If no collision with obstacle the point will be either p1 or p2
-			points = [(x_min,ol2),(ol1,y_min),(x_max,ol4),(ol3,y_max)]
+					y_min = slope*ol1 + y_int
+					y_max = slope*ol3 + y_int
 
-			# Check if the points determined are in the obstacle
-			for p in points:
-				if ((p[0] > max(p1[0],p2[0])) and (p[1] > max(p1[1],p2[1]))):
-					p = ( max(p1[0],p2[0]),max(p1[1],p2[1]) )
-				if ((p[0] < min(p1[0],p2[0])) and (p[1] < min(p1[1],p2[1]))):
-					p = ( min(p1[0],p2[0]),min(p1[1],p2[1]) )
-				# If they are, p1 and p2 are in NLOS, so add the obstacle that blocked it to the list
-				if( (p[0]>=ol1 and p[0]<=ol3) and (p[1]>=ol2 and p[1]<=ol4) ):
-					collisions[o].append((p[0],p[1]))
+					x_min = (ol2-y_int)/slope
+					x_max = (ol4-y_int)/slope
+
+				# Create list of intersection of lines
+				points = [(x_min,ol2),(ol1,y_min),(x_max,ol4),(ol3,y_max)]
+
+				# Check if the points created are in collision with the obstacle
+				for p in points:
+					dist1 = ((p1[0]-p[0])**2+(p1[1]-p[1])**2)**0.5
+					dist2 = ((p2[0]-p[0])**2+(p2[1]-p[1])**2)**0.5
+					tdist = ((p1[0]-p2[0])**2+(p1[1]-p2[1])**2)**0.5
+
+					# Make sure that the intersection point does not extend past the two points given
+					if (dist1 > tdist) or (dist2 > tdist):
+						if dist1 > dist2:
+							p = (p2[0],p2[1])
+						else:
+							p = (p1[0],p1[1])
+
+					# Check if updated intersection points are in an obstacle
+					if( (p[0]>=(ol1-0.001) and p[0]<=(ol3+0.001)) and (p[1]>=(ol2-0.001) and p[1]<=(ol4+0.001)) ):
+						collisions[o].append((p[0],p[1]))
 		
 		# Return list of obstacles that blocked p1 and p2
 		return collisions
@@ -87,8 +96,7 @@ class Environment:
 
 			if( (pos[0]>=ol1 and pos[0]<=ol3) and (pos[1]>=ol2 and pos[1]<=ol4) ):
 				return True
-			else:
-				return False
+		return False
 
 
 
@@ -102,7 +110,7 @@ def get_obstacles( environment_number ):
 	elif environment_number==1:
 		obstacles = {}
 		obstacles[0] = [1, 1, 2, 3]
-		obstacles[1] = [1.5, 6.5, 4.5, 7.0]
+		obstacles[1] = [1.5, 6.0, 4.5, 7.0]
 		return obstacles
 	else:
 		obstacles = {}
--- main.py
+++ main.py
@@ -32,11 +32,11 @@ if __name__ == "__main__":
 	beacon = BeaconNode(0.5,0.5,0)
 
 	## Setup variables to be used to create a distribution of points across environment
-	upper_x = env.boundaries['x'][1]-0.1
-	lower_x = env.boundaries['x'][0]+0.1
-	upper_y = env.boundaries['y'][1]-0.1
-	lower_y = env.boundaries['y'][0]+0.1
-	length = 20
+	upper_x = env.boundaries['x'][1]
+	lower_x = env.boundaries['x'][0]
+	upper_y = env.boundaries['y'][1]
+	lower_y = env.boundaries['y'][0]
+	length = 100
 
 	# Build linspace for x and y positions
 	beacon_positions_x = np.linspace(lower_x,upper_x,length)
@@ -46,57 +46,60 @@ if __name__ == "__main__":
 	for i in range(len(beacon_positions)):
 		pos = [beacon_positions_x[i-length*(i/length)],beacon_positions_y[i/length],0]
 		# Make sure the point created isn't inside an obstacle
-		if env.in_obstacle(pos):
-			continue
-		else:
-			beacon_positions[i] = pos
+		# if env.in_obstacle(pos):
+		# 	continue
+		# else:
+		beacon_positions[i] = pos
 	# From all positions, any that were inside obstacles need to be pruned
-	actual_positions = beacon_positions[~np.all(beacon_positions==0,axis=1)]
+	# actual_positions = beacon_positions[~np.all(beacon_positions==0,axis=1)]
 
 	error = []
+	test_error = {}
 	# For each actual position run the algorithm to obtain an estimated position
-	for b_pos in actual_positions:
+	for b_pos in beacon_positions:
 		# move beacon between tests
-		beacon.move_pos(b_pos[0],b_pos[1],b_pos[2])
 		distances = []
-		bp = beacon.get_pos()
-		print 'Beacon pos: ', bp
-		for a in anchors:
-			ap = a.get_pos()
-			collision = env.determine_NLOS([ap['x'],ap['y']],[bp['x'],bp['y']])
+		if env.in_obstacle(b_pos):
+			#print 'Obstacle pos: ', b_pos
+			test_error[str((b_pos[0],b_pos[1]))] = 0
 
-			distances.append(get_distance(a,bp,collision,env.dielectric))
+		else:
+			beacon.move_pos(b_pos[0],b_pos[1],b_pos[2])
+			bp = beacon.get_pos()
+			#print 'Beacon pos: ', bp
+			for a in anchors:
+				ap = a.get_pos()
+				collision = env.determine_NLOS([ap['x'],ap['y']],[bp['x'],bp['y']])
+				#print collision
 
-		error.append( abs( distances[3]-(((b_pos[0]-ap['x'])**2+(b_pos[1]-ap['y'])**2)**0.5) ) )
-		print 'Distance Measurements for each anchor: ', distances
+				distances.append(get_distance(a,bp,collision,env.dielectric))
+			test_error[str((b_pos[0],b_pos[1]))] = abs( distances[4]-(((b_pos[0]-anchors[4].get_pos()['x'])**2+(b_pos[1]-anchors[4].get_pos()['y'])**2)**0.5) )
+
+		#error.append( abs( distances[3]-(((b_pos[0]-ap['x'])**2+(b_pos[1]-ap['y'])**2)**0.5) ) )
+		#print 'Distance Measurements for each anchor: ', distances
 		
-	#print actual_positions
 
-	x, y = np.meshgrid(actual_positions[:,0],actual_positions[:,1])
-	#print y
-	#print x
+	y, x = np.meshgrid(beacon_positions_y,beacon_positions_x)
 
-	aerror = np.array(error)
+	aerror = np.zeros((length,length))
+
+	for i in range(length):
+		for j in range(length):
+			aerror[j,i] = test_error[str((beacon_positions_x[j],beacon_positions_y[i]))]
 	print aerror
-
-	# generate 2 2d grids for the x & y bounds
-	# y, x = np.mgrid[slice(-3, 3 + dy, dy),
- #                slice(-3, 3 + dx, dx)]
-	# print y
-	# print x
-
+	
 	plt.pcolor(x, y, aerror, cmap='RdBu', vmin=0, vmax=aerror.max())
 	plt.title('pcolor')
 	# set the limits of the plot to the limits of the data
-	plt.axis([actual_positions[:,0].min(), actual_positions[:,0].max(), actual_positions[:,1].min(), actual_positions[:,1].max()])
+	plt.axis([0,5,0,8])
 	plt.colorbar()
 	plt.show()
 
 	## Test position solver using Non-Linear Least Square algorithm
 	## Assume there are three fixed anchors
-	x_guess = numpy.array([3, 3])  # a position guess for mobile tag
-	p_FA = numpy.array([[0,0],[5,0],[2,5]])
-	d_M = numpy.array([[2,2],[-3,2],[0,-3]])
+	x_guess = np.array([3, 3])  # a position guess for mobile tag
+	p_FA = np.array([[0,0],[5,0],[2,5]])
+	d_M = np.array([[2,2],[-3,2],[0,-3]])
 	ps = Position_solver(x_guess,p_FA,d_M)
 	x_estimate = ps.NLLS
 	print "Estimate position of test1 using NLLS algorithm\n",x_estimate
