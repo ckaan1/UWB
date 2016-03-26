d4891f6854740636db37eb26e0b72ed00c339b14
qizong <qizongasd@gmail.com>
2016-03-26 18:14:09 -0400

    added Maximum Likelihood Alg

--- position_solver.py
+++ position_solver.py
@@ -12,7 +12,7 @@ class Position_solver:
 		self.NLLS = NLLS_opt(x_guess,p_FA,d_M)
 		
   
-    # NLLS function
+# NLLS function
 def func_1(x,p_FA,d_M) :
     import numpy as np
     num = len(p_FA)  # num - number of fixed anchors
@@ -33,3 +33,18 @@ def NLLS_opt(x_guess,p_FA,d_M):
     x = optimize.leastsq(func_1,x_guess,args=(p_FA,d_M))
 
     return x[0]
+   
+#Maximum Likelihood
+def func_2(x,p_FA,d_M):
+    import math
+    import numpy as np
+    num = len(d_M)
+    J_d = np.array([[0 for x in range(1)] for x in range(num)])
+    n_g = np.random.normal(0, 1, [1,num]) # Gaussian random noise
+    Q = np.diag(np.diag(n_g.T*n_g))
+    
+    for i in range(0,num):
+        J_d[i][0:1] = d_M[i] - np.linalg.norm(x-p_FA[i,0:2])
+    
+    J = np.dot(np.dot(J_d.T,np.linalg.inv(Q)),J_d)
+    return 1/math.sqrt((2*math.pi)**num*np.linalg.det(Q))*math.exp(-J/2)
