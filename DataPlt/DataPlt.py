2cba9fd3aa2a7fdc6fd0e5078980e2ab1a35521c
qizong <qizongasd@gmail.com>
2016-03-30 10:20:01 -0400

    Added mean and std calculation

--- DataPlt/DataPlt.py
+++ DataPlt/DataPlt.py
@@ -79,3 +79,55 @@ plt.title('Histogram of UWB Test Data (Outdoor, LOS, distance = 14 m)')
 plt.axis([14.0, 14.7, 0, 250])
 plt.grid(True)
 plt.show()
+
+# mean and standard deviation
+m_1 = np.mean(data_1)
+std_1 = np.std(data_1)
+m_2 = np.mean(data_2)
+std_2 = np.std(data_2)
+m_3 = np.mean(data_3)
+std_3 = np.std(data_3)
+m_4 = np.mean(data_4)
+std_4 = np.std(data_4)
+m_5 = np.mean(data_5)
+std_5 = np.std(data_5)
+
+mean = [m_1, m_2,m_3,m_4,m_5]
+Std = [std_1,std_2,std_3,std_4,std_5]
+width = 0.35
+mean_series = pd.Series.from_array(mean)  
+Std_series = pd.Series.from_array(Std)  
+x_labels = ['2 m', '4 m', '8 m', '12 m', '14 m']
+# Mean
+plt.figure()
+ax = mean_series.plot(kind='bar')
+ax.set_title("Mean of Test Data (LOS, Outdoor)")
+ax.set_xlabel("Distance")
+ax.set_ylabel("Mean")
+ax.set_xticklabels(x_labels)
+
+rects = ax.patches
+
+# Now make some labels
+labels = mean
+
+for rect, label in zip(rects, labels):
+    height = rect.get_height()
+    ax.text(rect.get_x() + rect.get_width()/2, height, label, ha='center', va='bottom')
+
+# STD
+plt.figure()
+ax = Std_series.plot(kind='bar')
+ax.set_title("STD of Test Data (LOS, Outdoor)")
+ax.set_xlabel("Distance")
+ax.set_ylabel("STD")
+ax.set_ylim(0,0.09)
+ax.set_xticklabels(x_labels)
+
+rects = ax.patches
+
+# Now make some labels
+labels = Std
+for rect, label in zip(rects, labels):
+    height = rect.get_height()
+    ax.text(rect.get_x() + rect.get_width()/2, height, label, ha='center', va='bottom')
